401b3dbe906597d0761713c76c7fd9a50dcd5f5e
calebpenning <calebpenning@mail.com>
2021-12-12 00:26:22 -0800

    working with structs in golang

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName string
+	contact contactInfo
+}
+
+func main() {
+	var alex person
+
+	fmt.Println(alex)
+}

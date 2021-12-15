d15264d5cb3649b75bb0c8b1d43ef63b60fca9f3
calebpenning <calebpenning@mail.com>
2021-12-14 22:15:20 -0800

    added in an 'update first name' func utilizing pointers

--- main.go
+++ main.go
@@ -10,16 +10,36 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName string
-	contact contactInfo
+	contactInfo
 }
 
 func main() {
-	jim := person{
+	// 
+	jim := person {
 		firstName: "Jim",
 		lastName: "P",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email: "Jim@gmoil.corm",
 			zipCode: 94550,
 		},
 	}
+
+	fmt.Println("I'm am Jim still")
+	jim.print()
+	// &variable == give me the memory address of this value
+	jimPointer := &jim
+	jimPointer.updateFirstName("JAMES>LOL")
+	jim.print()
+}
+
+// *pointer == give me the value that is sitting in that memory address
+func (pointerToPerson *person) updateFirstName(newFirstName string) {
+	// this line doesnt work? p.firstName = newFirstName
+	// we must utilize pointers
+
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
